26e07dc73987e32743f4b681dbda1a010e276ddd
vinodsai <vinodsana143@gmail.com>
2017-06-20 00:51:49 +0530

    home page created

---
+++ client/main.js
@@ -0,0 +1,16 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+Template.hello.onCreated(function() {
+
+});
+
+Template.hello.helpers({
+
+});
+
+Template.hello.events({
+
+});
