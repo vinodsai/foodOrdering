a0e1d4dbed4139b92900b673b696927a987d2218
vinodsai <vinodsana143@gmail.com>
2017-06-20 14:26:58 +0530

    some html changes

--- client/main.js
+++ client/main.js
@@ -1,16 +1,17 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
+
 import './main.html';
 
-Template.hello.onCreated(function() {
-
-});
-
-Template.hello.helpers({
-
-});
-
-Template.hello.events({
-
-});
+// Template.hello.onCreated(function() {
+//
+// });
+//
+// Template.hello.helpers({
+//
+// });
+//
+// Template.hello.events({
+//
+// });
